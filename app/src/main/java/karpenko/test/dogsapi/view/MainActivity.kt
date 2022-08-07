dd8619cfdb71e1a31b9a0b79dd2637d52cb07d1d
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-08-07 10:52:00 +0300

    create layers and fragments

--- app/src/main/java/karpenko/test/dogsapi/view/MainActivity.kt
+++ app/src/main/java/karpenko/test/dogsapi/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package karpenko.test.dogsapi
+package karpenko.test.dogsapi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import karpenko.test.dogsapi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
