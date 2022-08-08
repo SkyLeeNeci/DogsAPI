b34059b93c3f5c2982a4f04c42aa479204ab821e
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-08-08 09:07:33 +0300

    clean up and prepare project

--- app/src/main/java/karpenko/test/dogsapi/view/DogDetailFragment.kt
+++ app/src/main/java/karpenko/test/dogsapi/view/DogDetailFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import karpenko.test.dogsapi.R
+import karpenko.test.dogsapi.databinding.FragmentDogDetailBinding
+import karpenko.test.dogsapi.databinding.FragmentDogsListBinding
+import java.lang.RuntimeException
 
 class DogDetailFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentDogDetailBinding? = null
+    private val binding: FragmentDogDetailBinding
+        get() = _binding ?: throw RuntimeException("FragmentDogDetailBinding = null")
+
+    private val args by navArgs<DogDetailFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dog_detail, container, false)
+    ): View {
+        _binding = FragmentDogDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.dogInfoFragment.text = args.dogUuid.toString()
     }
 }
--- app/src/main/java/karpenko/test/dogsapi/view/DogsListFragment.kt
+++ app/src/main/java/karpenko/test/dogsapi/view/DogsListFragment.kt
@@ -5,21 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import karpenko.test.dogsapi.R
+import karpenko.test.dogsapi.databinding.FragmentDogsListBinding
+import java.lang.RuntimeException
 
 class DogsListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentDogsListBinding? = null
+    private val binding: FragmentDogsListBinding
+        get() = _binding ?: throw RuntimeException("FragmentDogsListBinding = null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dogs_list, container, false)
+    ): View {
+        _binding = FragmentDogsListBinding.inflate(inflater, container,false)
+        return binding.root
     }
-
 }
